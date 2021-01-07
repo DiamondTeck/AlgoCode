6e66c29231b35f1c3916eca71f0af186a8f4cb67
DiamondTeck <76523021+DiamondTeck@users.noreply.github.com>
2021-01-07 22:32:28 +0100

    Création d'une fonction qui change la % par mod et // par div

--- AlgoCode.py
+++ AlgoCode.py
@@ -1,3 +1,4 @@
+#marche avec print();input();input(objet,expression,"");expression
 def X_pos(ch1,ch2):
     x=[]
     i=0
@@ -17,8 +18,39 @@ def X_pos(ch1,ch2):
             x[j+1]=x[j]+x[j+1]+1
     return x
 
-
-
+def mod(sc):
+    w=sc
+    if "%" in sc:
+        b=[]
+        w=""
+        b=X_pos("%",sc)
+        v=list(sc)
+        for k in range (len(b)):
+            v.pop(b[len(b)-k-1]) 
+            v.insert(b[len(b)-k-1]," mod ")
+        k=0
+        for k in range(len(v)):
+            w=w+v[k]
+        return w
+def div(sc):
+    w=sc
+    if "//" in sc:
+        w=""
+        d=[]
+        d=X_pos("//",sc)
+        v=list(sc)
+        for k in range (len(d)):
+            v.pop(d[len(d)-k-1])
+            v.pop(d[len(d)-k-1])
+            v.insert(d[len(d)-1]," div ")
+        k=0
+        for k in range(len(v)):
+            w=w+v[k]
+    return w
+def m_d(s):
+    s=mod(s)
+    s=div(s)
+    return s
 
 
 #nom1=input("Donner Le nom du fichier en python\n")
@@ -28,7 +60,7 @@ def X_pos(ch1,ch2):
 nom1="py"
 nom2="py"
 al="al"
-
+v=[]
 i=0
 c=1
 z = []
@@ -37,65 +69,29 @@ with open(nom1+".py", "r") as fichier1, open(nom2+".txt", "w") as fichier2:
     fichier2.write("algorithme "+al+"\n")
     fichier2.write("debut\n")
     for s in fichier1:
-        x=[]
-        sc=[]
-        b=[]
-        d=[]
-        h=""
-        e=""
         if "\n" in s:
             s=s[0:len(s)-1]
-        if "," in s :
+        if "%" in s or "//" in s:
+            s=m_d(s)
             
-            x=X_pos(",",s)
-            x.insert(0,s.find("("))
-            x.append(s.find(")"))
-            y=s[0:s.find("(")+1]
-            for l in range (len(x)-1):
-                sc.append(s[x[l]+1:x[l+1]+1]) #sc.append(s[x[l]+1:x[l+1]]) : pour enlrver les ','
-            for j in range (len(sc)):
-                if ("'" != sc[j][0] and '"' != sc[j][0]):
-                    if "%" in sc[j]:
-                        b=X_pos("%",sc[j])
-                        d=X_pos("//",sc[j])
-                        v=list(sc[j])
-                        for k in range (len(b)):
-                            v.pop(b[len(b)-k-1]) 
-                            v.insert(b[len(b)-k-1]," mod ")
-                            for l in range(len(v)):
-                                h=h+v[l]
-                            
-                            sc[j]=h
-                    if "//" in sc[j]:
-                        k=0
-                        w=""
-                        for k in range (len(d)):
-                            v.pop(d[len(d)-k-1])
-                            v.pop(d[len(d)-k-1])
-                            v.insert(d[len(d)-1]," div ")
-                        k=0
-                        for k in range(len(v)):
-                            w=w+v[k]
-                        k=0
-                        for k in range(len(sc)):
-                            sc[j]=w
-                            e=e+sc[k]
-                    s=y+e
         if "print" in s :
             x = s.find("print")
             z.append("ecrire"+s[x+5:len(s)])
         elif "input()" in s :
             z.append("lire("+s[0]+")")
         elif "input(" in s and (s[s.find("input(")+6] == '"' or s[s.find("input(")+6] == "'"):
-            z.append("ecrire("+s[s.find("input(")+6:len(s)])
+            v=X_pos(")",s)
+            z.append("ecrire("+s[s.find("input(")+6:v[-1]])
             z.append("lire("+s[0]+")")
             i+=1
-        
-            
+            v=[]
+        elif "=" in s and "==" not in s and len(X_pos("=",s))==1:
+            s=(s[0:s.find("=")]+"←"+s[s.find("=")+1:len(s)]) #problem
+            z.append(s)
         else :
             if s != "" and s != " ":
                 print("ERROR on line ",c)
-            
+            i-=1
         i+=1
         c+=1
     for j in range (i):
