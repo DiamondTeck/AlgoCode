3d4bc39a1c0f59a59c089e1440e600a83ae7393c
DiamondTeck <76523021+DiamondTeck@users.noreply.github.com>
2021-01-09 15:54:26 +0100

    Correction et amélioration

--- AlgoCode.py
+++ AlgoCode.py
@@ -66,22 +66,38 @@ def A_pos(ch1,chP,prf):#trouver les position de prf dans la sous chaine (0,ch1,c
 nom1="py" # to del
 nom2="py" #to del
 al="al" #to del
-v=[]
 prf1=['"',",",'"',"+","-","*","/","%","//","mod","div","(",")","=","==","\n","\t","\b","\a"]
 prf2=["\n","\t","\b","\a"]    
 i=0 #nombre de z
 c=1 #nombre de lighne
 z=[]
 y=[]
+h=[]
 
 with open(nom1+".py", "r",encoding="utf-8") as fichier1, open(nom2+".txt", "w",encoding="utf-8") as fichier2:
     fichier2.write("algorithme "+al+"\n")
     fichier2.write("debut\n")
     for s in fichier1:
+        v=[]
+        chain=[]
+        t=""
+        if "\\n" in s:
+            s=s.replace("\\n","")
+        if "\n" in s:
+            s=s.replace("\n","")
+        if  '"' in s:
+            v=X_pos('"',s)
+            v.reverse()
+            for j in range (int(len(v)/2)):
+                chain.append(s[v[j*2+1]:v[j*2]])
+                s=s[0:v[j*2+1]]+"☼"+s[v[j*2]+1:len(s)]
+            v=[]
         y.append(s[0:2])
         for n in range (len(y)):
             mmm=len(y)-1-n
-            if "if" == y[mmm] and "\t" in y[len(y)-2] and "\t" not in s and "el" not in s:
+            print(y)
+            if "if" == y[mmm] and " " in y[len(y)-2] and "  " not in s and "el" not in s:
+                
                 l=0
                 for j in range (mmm):
                     v.append(y[mmm:len(y)])
@@ -96,18 +112,17 @@ with open(nom1+".py", "r",encoding="utf-8") as fichier1, open(nom2+".txt", "w",e
                                 i+=1  
                             break
         v=[]
-        if "\n" in s:
-            s=s[0:len(s)-1]
+       
         s=change("len","long",s)
         s=change("%"," mod ",s)
         s=change("//"," div ",s)
-        s=(change("print","ecrire",s))
-        s=(change("!=","≠",s))
-        s=(change(">=","≥",s))
-        s=(change("<=","≤",s))
-        s=(change("and","et",s))
-        s=(change("or","ou",s))
-        s=(change("not","non",s))
+        s=change("print","ecrire",s)
+        s=change("!=","≠",s)
+        s=change(">=","≥",s)
+        s=change("<=","≤",s)
+        s=change("and","et",s)
+        s=change("or","ou",s)
+        s=change("not","non",s)
         
         if "[" in s and ":" in s and "]" in s: #sous_chain
             v.append(X_pos("[",s))
@@ -161,24 +176,23 @@ with open(nom1+".py", "r",encoding="utf-8") as fichier1, open(nom2+".txt", "w",e
                     s=s[0:t+1]+"pos("+s[v[0][m]+6:v[1][m]]+","+var+s[v[1][m]:len(s)]            
                 
                     
-        if "input()" in s :#problem
-            y=d2_to_d1(A_pos(s.find("="),s,prf2))
-            if y!=[]:
-                x=max(y)+1
+        if "input()" in s :
+            h=d2_to_d1(A_pos(s.find("="),s,prf2))
+            if h!=[]:
+                x=max(h)+1
             else:
                 x=0
                 
             s=(s[0:x]+"lire("+s[x:s.find("=")]+")")
             
-        if "input(" in s and (s[s.find("input(")+6] != ")"):#problem
-            y=d2_to_d1(A_pos(s.find("="),s,prf2))
-            if y!=[]:
-                x=max(y)+1
+        if "input(" in s and (s[s.find("input(")+6] != ")"):
+            h=d2_to_d1(A_pos(s.find("="),s,prf2))
+            if h!=[]:
+                x=max(h)+1
             else:
                 x=0
             v=X_pos(")",s)
-            z.append(s[0:x]+"ecrire("+s[s.find("input(")+6:s.find(")")]+")")
-            i+=1
+            t=(s[0:x]+"ecrire("+s[s.find("input(")+6:s.find(")")]+")")
             s=s[0:x]+"lire("+s[x:s.find("=")]+")"
             
             v=[]
@@ -187,8 +201,23 @@ with open(nom1+".py", "r",encoding="utf-8") as fichier1, open(nom2+".txt", "w",e
         s=change("==","☺",s)
         s=change("=","←",s)
         s=change("☺","=",s)
+
+        if "☼" in s :
+            v=X_pos("☼",s)
+            v.reverse()
+            for j in range(len(v)):
+                s=s[0:v[j]]+chain[j]+'"'+s[v[j]+1:len(s)]
+        if "☼" in t :
+            v=X_pos("☼",t)
+            v.reverse()
+            for j in range(len(v)):
+                t=t[0:v[j]]+chain[j]+'"'+t[v[j]+1:len(t)]
         
-        if s!="" and s !=" ":#fin
+
+        if t!="":   #fin
+            z.append(t)
+            i+=1
+        if s!="" and s !=" ":
             z.append(s)
             i+=1
         c+=1#c
